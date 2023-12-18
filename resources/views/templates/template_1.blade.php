dd250ca04b92a06e2c4a57eb4c14974509730741
Asadullah Chowdhury Rahat <asadullah_chowdhury@outlook.com>
2023-12-18 13:00:40 +0600

    [t-001]-left side user image section completed

--- resources/views/templates/template_1.blade.php
+++ resources/views/templates/template_1.blade.php
@@ -1,1 +1,53 @@
-<h1>sdflskdfjslkdjflksdfklsd</h1>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Revealify - Template 1</title>
+</head>
+<body style="border: 1px solid; margin:0;padding:0;box-sizing:border-box;">
+
+
+{{--<div style="width: 250px;height: 450px;position: absolute;transform: rotate(45deg);background-color: #ffb700;z-index: 1;top:-200px"></div>--}}
+<table
+    style="float: left;max-width: 250px;border-right: 1px solid;background-color:#3a3939;padding: 40px;position:relative;z-index: 9999">
+    <thead>
+    <tr>
+        <!--User image-->
+        <th style="padding: 20px 20px 0;">
+            <img
+                style="width: 120px;height: 120px;object-fit: cover;border:5px solid #fff;border-top-left-radius: 70px;border-top-right-radius: 70px;border-bottom-right-radius:70px;"
+                src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="">
+        </th>
+        <!--User image-->
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <table style="border-left:1px solid #eee;padding-top: 20px">
+            <tbody>
+            <tr>
+                <td style="font-weight: 700;font-size: 22px;color: #fff;padding-left: 20px;font-family: 'Roboto', sans-serif;text-transform: uppercase">
+                    Contact Me
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </tr>
+    </tbody>
+</table>
+
+<table>
+    <thead>
+    <tr>
+        <th></th>
+    </tr>
+    </thead>
+</table>
+
+
+</body>
+</html>
