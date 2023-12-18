cdd0ae16c233a0aae4252a1b3e7f66fa662d0488
Asadullah Chowdhury Rahat <asadullah_chowdhury@outlook.com>
2023-12-18 15:35:08 +0600

    [t-001]-working on education section

--- resources/views/templates/template_1.blade.php
+++ resources/views/templates/template_1.blade.php
@@ -27,10 +27,11 @@
     </thead>
     <tbody>
     <tr>
-        <table style="border-left:1px solid #eee;padding-top: 20px;position: relative">
+        <table style="border-left:1px solid #eee;padding-top: 20px;position: relative;color: #fff;font-family: 'Raleway', sans-serif;">
             <tbody>
+            <!--Contact section title-->
             <tr>
-                <td style="font-weight: 700;font-size: 20px;color: #fff;padding-left: 20px;font-family: 'Roboto', sans-serif;text-transform: uppercase">
+                <td style="font-weight: 700;font-size: 20px;padding-left: 20px;font-family: 'Roboto', sans-serif;text-transform: uppercase">
                     <span
                         style="width: 5px;height: 25px;border-radius: 10px;background-color: #ffb700;position: absolute;left: -3px"></span>
 
@@ -38,9 +39,158 @@
                          src="./images/template-1/icon/profile-user.png" alt="user-icon">
                     Contact Me
                 </td>
-
-                <td></td>
             </tr>
+            <!--Contact section title-->
+
+            <!--Number-->
+            <tr>
+                <td style="padding-left: 20px;padding-top: 20px;font-size: 14px;position: relative">
+                    <span
+                        style="width: 5px;height: 10px;border-radius: 10px;background-color: #ffb700;position: absolute;left: -5px;top: 35px"></span>
+
+                    <img style="margin-right: 5px;width: 20px;height: 20px;position: relative;top: 15px"
+                         src="./images/template-1/icon/telephone-call.png" alt="telephone">
+                    <span>+1-718-310-5588</span>
+                    <br>
+                    <span style="padding-left: 29px">+1-718-310-5588</span>
+                </td>
+            </tr>
+            <!--Number-->
+
+            <!--Email/website-->
+            <tr>
+                <td style="padding-left: 20px;padding-top: 20px;font-size: 14px;position: relative">
+                    <span
+                        style="width: 5px;height: 10px;border-radius: 10px;background-color: #ffb700;position: absolute;left: -5px;top: 35px"></span>
+
+                    <img style="margin-right: 5px;width: 20px;height: 20px;position: relative;top: 15px"
+                         src="./images/template-1/icon/globe.png" alt="globe">
+                    <span>www.yourwebsite.com</span>
+                    <br>
+                    <span style="padding-left: 29px">yourinfo@email.com</span>
+                </td>
+            </tr>
+            <!--Email/website-->
+
+            <!--Location-->
+            <tr>
+                <td style="padding-left: 20px;padding-top: 20px;font-size: 14px;position: relative">
+                    <span
+                        style="width: 5px;height: 10px;border-radius: 10px;background-color: #ffb700;position: absolute;left: -5px;top: 35px"></span>
+                    <img style="margin-right: 5px;width: 20px;height: 20px;position: relative;top: 15px"
+                         src="./images/template-1/icon/map-pointer.png" alt="map pointer">
+                    <span>769 Prudence street</span>
+                    <br>
+                    <span style="padding-left: 29px">Lincoin Park, MI 48123</span>
+
+                </td>
+            </tr>
+            <!--Location-->
+
+            <!--Divider-->
+            <tr>
+                <td style="padding-top: 30px;padding-bottom:30px;padding-left:20px;font-size: 14px;position: relative;letter-spacing: 2px">
+                    -------------------------------
+                </td>
+            </tr>
+            <!--Divider-->
+
+            <!--Reference section title-->
+            <tr>
+                <td style="font-weight: 700;font-size: 20px;padding: 20px;font-family: 'Roboto', sans-serif;text-transform: uppercase">
+                    <span
+                        style="width: 5px;height: 25px;border-radius: 10px;background-color: #ffb700;position: absolute;left: -3px"></span>
+
+                    <img style="margin-right: 5px;width: 24px;height: 24px;position: relative;top: 5px"
+                         src="./images/template-1/icon/users.png" alt="users">
+                    References
+                </td>
+            </tr>
+            <!--Reference section title-->
+
+            <!--References-->
+            <tr>
+                <td style="font-weight: 700;font-size:16px;padding-left:20px;position: relative">
+                    <span
+                        style="width: 5px;height: 10px;border-radius: 10px;background-color: #ffb700;position: absolute;left: -5px;top: 3px"></span>
+
+                    Darwin B. Magana
+                </td>
+            </tr>
+
+            <tr>
+                <td style="font-size: 14px;padding-left: 20px;">2813 Shobe Lane Mancos. CO.</td>
+            </tr>
+
+            <tr>
+                <td style="font-size: 14px;padding-left: 20px;">Tel: +1-718-310-5588</td>
+            </tr>
+
+            <tr>
+                <td style="font-size: 14px;padding-left: 20px;">Email: yourinfo@email.com</td>
+            </tr>
+
+            <tr>
+                <td style="font-weight: 700;font-size:16px;padding-left:20px;padding-top:20px;position: relative">
+                    <span
+                        style="width: 5px;height: 10px;border-radius: 10px;background-color: #ffb700;position: absolute;left: -5px;top: 25px"></span>
+
+                    Robert j. Belvin
+                </td>
+            </tr>
+
+            <tr>
+                <td style="font-size: 14px;padding-left: 20px;">2813 Shobe Lane Mancos. CO.</td>
+            </tr>
+
+            <tr>
+                <td style="font-size: 14px;padding-left: 20px;">Tel: +1-718-310-5588</td>
+            </tr>
+
+            <tr>
+                <td style="font-size: 14px;padding-left: 20px;">Email: yourinfo@email.com</td>
+            </tr>
+            <!--References-->
+
+            <!--Divider-->
+            <tr>
+                <td style="padding-top: 30px;padding-bottom:30px;padding-left:20px;font-size: 14px;position: relative;letter-spacing: 2px">
+                    -------------------------------
+                </td>
+            </tr>
+            <!--Divider-->
+
+            <!--Education section title-->
+            <tr>
+                <td style="font-weight: 700;font-size: 20px;padding: 20px;font-family: 'Roboto', sans-serif;text-transform: uppercase">
+                    <span
+                        style="width: 5px;height: 25px;border-radius: 10px;background-color: #ffb700;position: absolute;left: -3px"></span>
+
+                    <img style="margin-right: 5px;width: 24px;height: 24px;position: relative;top: 5px"
+                         src="./images/template-1/icon/education.png" alt="education">
+                    Education
+                </td>
+            </tr>
+            <!--Education section title-->
+
+{{--            <tr>--}}
+{{--                <td style="font-weight: 700;font-size:16px;padding-left:20px;padding-top:20px;position: relative">--}}
+{{--                    <span--}}
+{{--                        style="width: 5px;height: 10px;border-radius: 10px;background-color: #ffb700;position: absolute;left: -5px;top: 25px"></span>--}}
+
+{{--                    Stanford University--}}
+{{--                </td>--}}
+{{--            </tr>--}}
+
+{{--            <tr>--}}
+{{--                <td style="font-size: 14px;padding-left: 20px;">Master Digree Graduate</td>--}}
+{{--            </tr>--}}
+
+{{--            <tr>--}}
+{{--                <td style="font-size: 14px;padding-left: 20px;">2011 - 2013</td>--}}
+{{--            </tr>--}}
+
+
             </tbody>
         </table>
     </tr>
