73d8a67dd52f9787563f8af5b2e43eb1340443c7
Noyon Ahammed <noyon.redishketch@gmail.com>
2023-12-19 13:07:57 +0600

    header section complete

---
+++ resources/views/templates/template_4.blade.php
@@ -0,0 +1,283 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Theme Four</title>
+</head>
+<body>
+<table style="width: 100%;">
+    <tbody>
+    <tr>
+        <td style="width: 35%">
+            <div style="padding: 20px 0; text-align: center;">
+                <img src="./images/template-four/images/person.jpg" alt=""
+                     style="height: 150px; width: 150px; border-radius: 50%;">
+            </div>
+        </td>
+        <td style="width: 65%">
+            <div style="padding-left: 30px; color: #434343;">
+                <div style="font-size: 30px; line-height: 0">Jennifer</div>
+                <div style="font-size: 30px; font-weight: bold; margin-bottom: 5px;">Anderson</div>
+                <div>User experience designer</div>
+            </div>
+        </td>
+    </tr>
+    <tr>
+        <td style="width: 35%; vertical-align: top;">
+            <div style="padding: 30px 0 40px 30px; background-color: #404042; border-radius: 0 40px 0 0">
+                {{--contact--}}
+                <div style="color: #ffffff; margin-bottom: 40px;">
+                    <div style="border-bottom: 2px solid #b4b4b6; margin-bottom: 15px;">
+                        <div
+                            style="height: 40px; width: 40px; text-align: center; border-radius: 50%; background-color: #ffffff;display: inline-block;">
+                            <img src="./images/template-four/icons/phone-book.png" alt=""
+                                 style="position:relative; top: 7px;">
+                        </div>
+                        <div
+                            style="text-transform: uppercase; margin-left: 10px; font-weight: bold; display: inline-block;position:relative; top: -9px; font-size: 18px;">
+                            contact
+                        </div>
+                    </div>
+                    <div style="margin-bottom: 10px; font-size: 12px;">
+                        <div>Address</div>
+                        <div>12th Avenue Street Australia 40000</div>
+                    </div>
+                    <div style="margin-bottom: 10px; font-size: 12px;">
+                        <div>Mobile:</div>
+                        <div>02800200</div>
+                    </div>
+                    <div style="margin-bottom: 10px; font-size: 12px;">
+                        <div>Email:</div>
+                        <div>janderson@gmail.com</div>
+                    </div>
+                    <div style="margin-bottom: 10px; font-size: 12px;">
+                        <div>Website:</div>
+                        <div>janderson.com</div>
+                    </div>
+                    <div style="margin-bottom: 10px; font-size: 12px;">
+                        <div>Skype:</div>
+                        <div>username</div>
+                    </div>
+                </div>
+
+                {{--skills--}}
+                <div style="color: #ffffff; margin-bottom: 40px;">
+                    <div style="border-bottom: 2px solid #b4b4b6; margin-bottom: 15px;">
+                        <div
+                            style="height: 40px; width: 40px; text-align: center; border-radius: 50%; background-color: #ffffff;display: inline-block;">
+                            <img src="./images/template-four/icons/phone-book.png" alt=""
+                                 style="position:relative; top: 7px;">
+                        </div>
+                        <div
+                            style="text-transform: uppercase; margin-left: 10px; font-weight: bold; display: inline-block;position:relative; top: -9px; font-size: 18px;">
+                            skills
+                        </div>
+                    </div>
+                    <div style="margin-bottom: 10px; font-size: 12px;">
+                        <div>Communication</div>
+                    </div>
+                    <div style="margin-bottom: 10px; font-size: 12px;">
+                        <div>Creativity</div>
+                    </div>
+                    <div style="margin-bottom: 10px; font-size: 12px;">
+                        <div>Teamwork</div>
+                    </div>
+                    <div style="margin-bottom: 10px; font-size: 12px;">
+                        <div>Organization</div>
+                    </div>
+                    <div style="margin-bottom: 10px; font-size: 12px;">
+                        <div>Teamplayer</div>
+                    </div>
+                    <div style="margin-bottom: 10px; font-size: 12px;">
+                        <div>Leadership</div>
+                    </div>
+                </div>
+
+                {{--references--}}
+                <div style="color: #ffffff;">
+                    <div style="border-bottom: 2px solid #b4b4b6; margin-bottom: 15px;">
+                        <div
+                            style="height: 40px; width: 40px; text-align: center; border-radius: 50%; background-color: #ffffff;display: inline-block;">
+                            <img src="./images/template-four/icons/phone-book.png" alt=""
+                                 style="position:relative; top: 7px;">
+                        </div>
+                        <div
+                            style="text-transform: uppercase; margin-left: 10px; font-weight: bold; display: inline-block;position:relative; top: -9px; font-size: 18px;">
+                            references
+                        </div>
+                    </div>
+                    <div style="margin-bottom: 20px; font-size: 12px;">
+                        <div style="font-size: 14px; font-weight: bold; margin-bottom: 10px;">William Kleiman</div>
+                        <div>Director, Matrix Media Limited</div>
+                        <div>Phone: +555 123 5566</div>
+                        <div>Email: williamkleiman@gmail.com</div>
+                    </div>
+                    <div style="font-size: 12px;">
+                        <div style="font-size: 14px; font-weight: bold; margin-bottom: 10px;">Jensen Smith</div>
+                        <div>Web developer, Design mate LTD</div>
+                        <div>Phone: +555 123 4455</div>
+                        <div>Email: jensonsmith@gmail.com</div>
+                    </div>
+                </div>
+            </div>
+        </td>
+        <td style="width: 65%; vertical-align: top;">
+            <div style="padding: 30px 0 5px 30px;">
+                {{--about--}}
+                <div style="margin-bottom: 20px;">
+                    <div style="border-bottom: 2px solid #b4b4b6; margin-bottom: 10px;">
+                        <div
+                            style="height: 40px; width: 40px; text-align: center; border-radius: 50%; background-color: #3d3d3d;display: inline-block;">
+                            <img src="./images/template-four/icons/suitcase.png" alt=""
+                                 style="position:relative; top: 7px;">
+                        </div>
+                        <div
+                            style="text-transform: uppercase; margin-left: 10px; font-weight: bold; display: inline-block;position:relative; top: -9px; font-size: 18px;">
+                            About Me
+                        </div>
+                    </div>
+                    <div style="margin-bottom: 10px; font-size: 12px;">
+                        <div style="color: #adadad;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi,
+                            asperiores dolor dolorem
+                            dolorum exercitationem explicabo facere fugit id laborum minima, nihil quaerat quidem quis
+                            quod temporibus voluptate voluptatem. Cumque, doloremque eligendi ipsa nam nisi non
+                            quibusdam quis! A dicta ipsa provident ullam vero! Aliquam aliquid architecto assumenda
+                            autem beatae blanditiis commodi consequuntur corporis dolor doloremque ducimus eos error et
+                            eum excepturi fugiat in iste itaque iure labore magni maxime mollitia nemo nesciunt numquam
+                            omnis perferendis quibusdam quis ratione repellat reprehenderit sapiente tempore temporibus,
+                            veniam veritatis voluptas voluptatibus. Ea eum fugit iste rem ut? Consequuntur culpa.
+                        </div>
+                    </div>
+                </div>
+
+                {{--experiences--}}
+                <div style="margin-bottom: 10px;">
+                    <div style="border-bottom: 2px solid #b4b4b6; margin-bottom: 10px;">
+                        <div
+                            style="height: 40px; width: 40px; text-align: center; border-radius: 50%; background-color: #3d3d3d;display: inline-block;">
+                            <img src="./images/template-four/icons/suitcase.png" alt=""
+                                 style="position:relative; top: 7px;">
+                        </div>
+                        <div
+                            style="text-transform: uppercase; margin-left: 10px; font-weight: bold; display: inline-block;position:relative; top: -9px; font-size: 18px;">
+                            Experiences
+                        </div>
+                    </div>
+                    <div style="margin-bottom: 10px; font-size: 12px;">
+                        <table style="width: 100%;">
+                            <tbody>
+                            <tr>
+                                <td style="width: 30%; color: #444444; vertical-align: top;">
+                                    <div style="margin-bottom: 5px; font-weight: bold;">Graphic Designer</div>
+                                    <div>2015 -2017</div>
+                                </td>
+                                <td style="width: 70%; vertical-align: top;">
+                                    <div style="margin-bottom: 5px; color: #444444; font-weight: bold;">Soft Design Studio</div>
+                                    <div style="color: #adadad;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, inventore,
+                                        nihil! Accusantium eos odit quasi velit. At aut, dolore doloremque, dolorum ipsa
+                                        natus praesentium, quasi quibusdam quo sunt tempore vitae!
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2">
+                                    <div style="margin-bottom: 5px"></div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="width: 30%; color: #444444; vertical-align: top;">
+                                    <div style="margin-bottom: 5px; font-weight: bold;">Web Designer</div>
+                                    <div>2015 -2017</div>
+                                </td>
+                                <td style="width: 70%; vertical-align: top;">
+                                    <div style="margin-bottom: 5px; color: #444444; font-weight: bold;">Web Tech Ltd</div>
+                                    <div style="color: #adadad;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, inventore,
+                                        nihil! Accusantium eos odit quasi velit. At aut, dolore doloremque, dolorum ipsa
+                                        natus praesentium, quasi quibusdam quo sunt tempore vitae!
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2">
+                                    <div style="margin-bottom: 5px"></div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="width: 30%; color: #444444; vertical-align: top;">
+                                    <div style="margin-bottom: 5px; font-weight: bold;">Lead Web Designer</div>
+                                    <div>2015 -2017</div>
+                                </td>
+                                <td style="width: 70%; vertical-align: top;">
+                                    <div style="margin-bottom: 5px; color: #444444; font-weight: bold;">Dev Creative Solution</div>
+                                    <div style="color: #adadad;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, inventore,
+                                        nihil! Accusantium eos odit quasi velit. At aut, dolore doloremque, dolorum ipsa
+                                        natus praesentium, quasi quibusdam quo sunt tempore vitae!
+                                    </div>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+                {{--education--}}
+                <div>
+                    <div style="border-bottom: 2px solid #b4b4b6; margin-bottom: 10px;">
+                        <div
+                            style="height: 40px; width: 40px; text-align: center; border-radius: 50%; background-color: #3d3d3d;display: inline-block;">
+                            <img src="./images/template-four/icons/suitcase.png" alt=""
+                                 style="position:relative; top: 7px;">
+                        </div>
+                        <div
+                            style="text-transform: uppercase; margin-left: 10px; font-weight: bold; display: inline-block;position:relative; top: -9px; font-size: 18px;">
+                            Education
+                        </div>
+                    </div>
+                    <div style="margin-bottom: 10px; font-size: 12px;">
+                        <table style="width: 100%;">
+                            <tbody>
+                            <tr>
+                                <td style="width: 30%; color: #444444; vertical-align: top;">
+                                    <div style="margin-bottom: 5px; font-weight: bold;">University of Lorem</div>
+                                    <div>2008 -2010</div>
+                                </td>
+                                <td style="width: 70%; vertical-align: top;">
+                                    <div style="margin-bottom: 5px; color: #444444; font-weight: bold;">Certificate of Web Training</div>
+                                    <div style="color: #adadad;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, inventore,
+                                        nihil! Accusantium eos odit quasi velit. At aut, dolore doloremque, dolorum ipsa
+                                        natus praesentium, quasi quibusdam quo sunt tempore vitae!
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2">
+                                    <div style="margin-bottom: 5px"></div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="width: 30%; color: #444444; vertical-align: top;">
+                                    <div style="margin-bottom: 5px; font-weight: bold;">University of Lorem</div>
+                                    <div>2015 -2017</div>
+                                </td>
+                                <td style="width: 70%; vertical-align: top;">
+                                    <div style="margin-bottom: 5px; color: #444444; font-weight: bold;">Bachelor of Art Director</div>
+                                    <div style="color: #adadad;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, inventore,
+                                        nihil! Accusantium eos odit quasi velit. At aut, dolore doloremque, dolorum ipsa
+                                        natus praesentium, quasi quibusdam quo sunt tempore vitae!
+                                    </div>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </td>
+    </tr>
+    </tbody>
+</table>
+</body>
+</html>
