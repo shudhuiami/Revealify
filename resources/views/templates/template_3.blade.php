d7e9c066d9c43587474d1bcfc3617e7081119fe5
Asadullah Chowdhury Rahat <asadullah_chowdhury@outlook.com>
2023-12-19 12:12:48 +0600

    [t-003]-working on template three header

---
+++ resources/views/templates/template_3.blade.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Revealify - Template 3</title>
+</head>
+<body
+    style="margin: 0;padding: 10px;box-sizing: border-box;font-size: 14px;font-family: sans-serif;background-color: #313030;">
+
+<!--Main content box start-->
+<div style="background-color: #fff;border-radius: 40px">
+    <table
+        style="width: 100%;background-color: #eee;padding: 30px;border-top-left-radius: 40px;border-top-right-radius: 40px;position: relative">
+        <tbody>
+        <tr>
+            <td style="">
+                <div
+                    style="display:inline-block;padding: 5px;border-radius: 50%;background-color:#313030;margin-right: 5px">
+                    <img style="width: 20px;height: 20px;" src="./images/template-3/man.png" alt="man">
+                </div>
+
+                <div style="font-size: 24px;font-weight: 700;color: #313030;display: inline-block">About Me</div>
+
+                <div style="margin-top: 10px;line-height: 1.5">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, atque aut dolor dolore,
+                    dolorum
+                    enim eos et excepturi, fugit illum inventore ipsum iste laboriosam maiores minima provident rem
+                    suscipit
+                    velit.
+                </div>
+
+            </td>
+            <td style="font-size: 35px;font-weight: 700;color: #313030;text-transform: uppercase;width: 300px;vertical-align: top;text-align: right;">
+                Noel Taylor
+                <div style="font-weight: 400;font-size: 16px">Graphic and web designer</div>
+                <div
+                    style="width: 220px;height: 220px;border-radius: 50%;border: 5px solid #fff;position: absolute; right: 30px; top: 110px"></div>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+<!--Main content box end  -->
+
+</body>
+</html>
