7e7cbe5aa55409e3a96aef4de322c8ceb5d3f134
Asadullah Chowdhury Rahat <asadullah_chowdhury@outlook.com>
2023-12-20 19:00:19 +0600

    [theme-005]-working on contact section

--- resources/views/templates/template_5.blade.php
+++ resources/views/templates/template_5.blade.php
@@ -24,14 +24,54 @@
     <tr>
         <!--Left body section start-->
         <td style="background: #e7f3ff;height: 99.8%;width: 270px;vertical-align: top">
-            <table style="padding: 15px;vertical-align: top;border: 1px solid">
+            <table style="padding: 15px;vertical-align: top;">
                 <tbody>
                 <tr>
                     <td style="padding:20px 0;">
                         <div
                             style="width: 130px;height: 130px;border: 3px solid #0067de; border-radius: 50%;margin: 0 auto;overflow: hidden">
-                            <img style="width: 130px;height: 130px;object-fit: contain;border-radius: 50%" src="./images/template-5/image/user-profile.jpg" alt="user profile">
+                            <img style="width: 130px;height: 130px;object-fit: contain;border-radius: 50%"
+                                 src="./images/template-5/image/user-profile.jpg" alt="user profile">
                         </div>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <table>
+                            <tr>
+                                <td style="width: 50px;padding-bottom: 20px;">
+                                    <div
+                                        style=";border-radius: 50%;background-color: #0067de;padding:10px;display: inline-block">
+                                        <img style="width: 15px;height: 15px;" src="./images/template-5/icon/user.png"
+                                             alt="user">
+                                    </div>
+                                </td>
+
+                                <td style="padding-bottom: 20px;font-size: 18px;font-weight: 700;color: #0067de;text-transform: uppercase">
+                                    About Me
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="padding-bottom: 30px;"></td>
+                                <td style="color: #7e7e7e;line-height: 1.5;padding-bottom: 30px;">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                    Ab alias amet aut, dicta doloremque dolorum eligendi facilis
+                                    itaque laboriosam minus necessitatibus nemo nobis non obcaecati
+                                    praesentium, reprehenderit sit soluta ullam.
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td style="width: 50px;padding-bottom: 20px;">
+
+                                </td>
+
+                                <td style="padding-bottom: 20px;font-size: 18px;font-weight: 700;color: #0067de;text-transform: uppercase">
+                                    Contact
+                                </td>
+                            </tr>
+                        </table>
                     </td>
                 </tr>
                 </tbody>
