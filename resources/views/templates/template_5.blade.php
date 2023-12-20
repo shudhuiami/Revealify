218ea637efc913bac4af4ad15a1a741610b80646
Asadullah Chowdhury Rahat <asadullah_chowdhury@outlook.com>
2023-12-20 18:39:41 +0600

    [theme-005]-layout created and image and icon added and profile image added

---
+++ resources/views/templates/template_5.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Revealify - Template 5</title>
+    <style>
+        @page {
+            margin: 0;
+        }
+
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+    </style>
+</head>
+<body style="margin: 0;padding: 0;box-sizing: border-box;font-size: 12px;font-family: sans-serif">
+<!--main body section start-->
+<table>
+    <tbody>
+    <tr>
+        <!--Left body section start-->
+        <td style="background: #e7f3ff;height: 99.8%;width: 270px;vertical-align: top">
+            <table style="padding: 15px;vertical-align: top;border: 1px solid">
+                <tbody>
+                <tr>
+                    <td style="padding:20px 0;">
+                        <div
+                            style="width: 130px;height: 130px;border: 3px solid #0067de; border-radius: 50%;margin: 0 auto;overflow: hidden">
+                            <img style="width: 130px;height: 130px;object-fit: contain;border-radius: 50%" src="./images/template-5/image/user-profile.jpg" alt="user profile">
+                        </div>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </td>
+        <!--Left body section end  -->
+
+        <!--Right body section start-->
+        <td style="background: yellow"></td>
+        <!--Right body section end  -->
+    </tr>
+    </tbody>
+</table>
+<!--main body section end  -->
+</body>
+</html>
