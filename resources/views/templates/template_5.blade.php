0d72e9d1473af592350fd752880b582fcca11e93
Asadullah Chowdhury Rahat <asadullah_chowdhury@outlook.com>
2023-12-21 12:57:59 +0600

    [theme-005]-UI has been completed.

--- resources/views/templates/template_5.blade.php
+++ resources/views/templates/template_5.blade.php
@@ -21,7 +21,7 @@
         }
     </style>
 </head>
-<body style="margin: 0;padding: 0;box-sizing: border-box;font-size: 12px;font-family: sans-serif">
+<body style="margin: 0;padding: 0;box-sizing: border-box;font-size: 13px;font-family: sans-serif">
 <!--main body section start-->
 <table>
     <tbody>
@@ -33,8 +33,8 @@
                 <tr>
                     <td style="padding:20px 0;">
                         <div
-                            style="width: 130px;height: 130px;border: 3px solid {{$theme_color}}; border-radius: 50%;margin: 0 auto;overflow: hidden">
-                            <img style="width: 130px;height: 130px;object-fit: contain;border-radius: 50%"
+                            style="width: 150px;height: 150px;border: 3px solid {{$theme_color}}; border-radius: 50%;margin: 0 auto;overflow: hidden">
+                            <img style="width: 150px;height: 150px;object-fit: contain;border-radius: 50%"
                                  src="./images/template-5/image/user-profile.jpg" alt="user profile">
                         </div>
                     </td>
@@ -63,7 +63,8 @@
                                     Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                                     Ab alias amet aut, dicta doloremque dolorum eligendi facilis
                                     itaque laboriosam minus necessitatibus nemo nobis non obcaecati
-                                    praesentium, reprehenderit sit soluta ullam. lorem
+                                    praesentium, reprehenderit sit soluta ullam. Lorem ipsum dolor sit amet, consectetur
+                                    adipisicing elit. Assumenda aut commodi dolorem ea inventore ipsa libero magni nam
                                 </td>
                             </tr>
                             <!--About me end  -->
@@ -80,22 +81,25 @@
                             <tr>
                                 <td style="width: 50px;">
                                     <div
-                                        style=";border-radius: 50%;background-color: {{$theme_color}};padding:15px 6.5px;display: inline-block">
-                                        <img style="width: 15px;height: 15px;position: relative;left: 3px;margin-bottom: 2px;"
-                                             src="./images/template-5/icon/phone-call.png"
-                                             alt="phone-call">
+                                        style=";border-radius: 50%;background-color: {{$theme_color}};padding:15px 6.5px;display: inline-block;position: relative;top: -5px">
+                                        <img
+                                            style="width: 15px;height: 15px;position: relative;left: 3px;margin-bottom: 2px;"
+                                            src="./images/template-5/icon/phone-call.png"
+                                            alt="phone-call">
                                         <br>
                                         <br>
 
-                                        <img style="width: 15px;height: 15px;position: relative;left: 3px;margin-bottom: 2px;"
-                                             src="./images/template-5/icon/email.png"
-                                             alt="email">
+                                        <img
+                                            style="width: 15px;height: 15px;position: relative;left: 3px;margin-bottom: 2px;"
+                                            src="./images/template-5/icon/email.png"
+                                            alt="email">
                                         <br>
                                         <br>
 
-                                        <img style="width: 15px;height: 15px;position: relative;left: 3px;margin-bottom: 2px;"
-                                             src="./images/template-5/icon/home.png"
-                                             alt="home">
+                                        <img
+                                            style="width: 15px;height: 15px;position: relative;left: 3px;margin-bottom: 2px;"
+                                            src="./images/template-5/icon/home.png"
+                                            alt="home">
                                     </div>
 
                                 </td>
@@ -173,7 +177,166 @@
         <!--Left body section end  -->
 
         <!--Right body section start-->
-        <td style="background: yellow"></td>
+        <td style="vertical-align:top;padding: 50px;">
+            <table>
+                <tbody>
+                <tr>
+                    <td style="font-size: 50px;font-weight: 400;padding-bottom: 70px;position: relative">
+                        <span style="color: {{$theme_color}};font-weight: 700;">Areata</span> Base
+                        <br>
+                        <div style="font-size: 30px"> UI Designer</div>
+
+                        <div
+                            style="position: absolute;width: 20px;height: 150px;border-radius: 50%;background-color: {{$theme_color}};right: -50px;top: -20px"></div>
+                    </td>
+                </tr>
+
+                <!--Experience section start-->
+                <tr>
+                    <td style="padding-bottom: 30px;font-size: 18px;font-weight: 700;color: {{$theme_color}};text-transform: uppercase">
+                        Experience
+                    </td>
+                </tr>
+
+                <tr>
+                    <td style="padding-bottom: 30px;">
+                        <div style="text-transform: uppercase;margin-bottom: 10px;">Senior UX Designer - Present</div>
+                        <div style="margin-bottom: 10px;color: #313030">Company Name/ Location</div>
+                        <div style="margin-right: 10px;line-height: 1.5;color: #313030">Lorem ipsum dolor sit amet,
+                            consectetur adipisicing elit. Aut
+                            cupiditate debitis,consectetur adipisicing elit. Aut
+                            cupiditate debitis,
+                            doloremque eaque expedita fugiat laudantium, nihil
+                            nostrum odit quibusdam quos rem velit?
+                            doloremque eaque expedita fugiat laudantium, nihil
+                            Aut illo maxime ratione sit. Saepe, vero?
+                        </div>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td style="padding-bottom: 20px;">
+                        <div style="text-transform: uppercase;margin-bottom: 10px;">Creative User interface Desinger -
+                            2016
+                        </div>
+                        <div style="margin-bottom: 10px;color: #313030">Company Name/ Location</div>
+                        <div style="margin-right: 10px;line-height: 1.5;color: #313030">Lorem ipsum dolor sit amet,
+                            consectetur adipisicing elit. Aut
+                            cupiditate debitis,consectetur adipisicing elit. Aut
+                            cupiditate debitis,
+                            doloremque eaque expedita fugiat laudantium, nihil
+                            nostrum odit quibusdam quos rem velit?
+                            doloremque eaque expedita fugiat laudantium, nihil
+                            Aut illo maxime ratione sit. Saepe, vero?
+
+                        </div>
+                    </td>
+                </tr>
+
+                <!--Experience section end  -->
+
+
+                <!--Skills section start-->
+                <tr>
+                    <td style="padding-bottom: 30px;padding-top: 30px;font-size: 18px;font-weight: 700;color: {{$theme_color}};text-transform: uppercase">
+                        Professional Skills
+                    </td>
+                </tr>
+
+                <tr>
+                    <td style="padding-bottom: 30px;">
+                        <table>
+                            <tbody>
+                            <tr>
+                                <td style="text-transform: uppercase;padding-bottom: 30px;">Photoshop</td>
+
+                                <td style="padding-bottom: 30px;">
+                                    <div
+                                        style="width: 200px;height: 6px;background-color: {{$theme_color}};border-radius: 10px;position: relative">
+                                        <div
+                                            style="position: absolute;top:-6px;right: 30px;width: 18px;height: 18px;border-radius: 50%;background-color: {{$theme_color}};"></div>
+                                    </div>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td style="text-transform: uppercase;padding-bottom: 30px;">Illustrator</td>
+
+                                <td style="padding-bottom: 30px;">
+                                    <div
+                                        style="width: 200px;height: 6px;background-color: {{$theme_color}};border-radius: 10px;position: relative">
+                                        <div
+                                            style="position: absolute;top:-6px;right: 40px;width: 18px;height: 18px;border-radius: 50%;background-color: {{$theme_color}};"></div>
+                                    </div>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td style="text-transform: uppercase;padding-bottom: 30px;">Indesign</td>
+
+                                <td style="padding-bottom: 30px;">
+                                    <div
+                                        style="width: 200px;height: 6px;background-color: {{$theme_color}};border-radius: 10px;position: relative">
+                                        <div
+                                            style="position: absolute;top:-6px;right: 45px;width: 18px;height: 18px;border-radius: 50%;background-color: {{$theme_color}};"></div>
+                                    </div>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td style="text-transform: uppercase;padding-bottom: 30px;">After Effect</td>
+
+                                <td style="padding-bottom: 30px;">
+                                    <div
+                                        style="width: 200px;height: 6px;background-color: {{$theme_color}};border-radius: 10px;position: relative">
+                                        <div
+                                            style="position: absolute;top:-6px;right: 40px;width: 18px;height: 18px;border-radius: 50%;background-color: {{$theme_color}};"></div>
+                                    </div>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td style="text-transform: uppercase;padding-bottom: 30px;">Sketch</td>
+
+                                <td style="padding-bottom: 30px;">
+                                    <div
+                                        style="width: 200px;height: 6px;background-color: {{$theme_color}};border-radius: 10px;position: relative">
+                                        <div
+                                            style="position: absolute;top:-6px;right: 60px;width: 18px;height: 18px;border-radius: 50%;background-color: {{$theme_color}};"></div>
+                                    </div>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+                <!--Skills section end  -->
+
+                <!--Interest section start-->
+                <tr>
+                    <td style="padding-bottom: 30px;font-size: 18px;font-weight: 700;color: {{$theme_color}};text-transform: uppercase">
+                        Interests
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <table style="color: #313030">
+                            <tbody>
+                            <tr>
+                                <td>Gaming</td>
+                                <td>Traveling</td>
+                                <td>Singing</td>
+                                <td>Sketching</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+                <!--Interest section end  -->
+                </tbody>
+            </table>
+        </td>
         <!--Right body section end  -->
     </tr>
     </tbody>
