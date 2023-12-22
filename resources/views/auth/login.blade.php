0155ebdcf11fd7fe5d53159c9f03868568f129c6
Asadullah Chowdhury Rahat <asadullah_chowdhury@outlook.com>
2023-12-22 18:16:04 +0600

    responsive update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,11 +13,11 @@
                 <!--Authentication form start-->
                 <form action="" class="auth-form p-sm-5 p-4">
 
-                    <div class="logo text-uppercase fs-4 fw-bold my-5 pb-4">Revealify</div>
+                    <div class="logo text-uppercase fs-4 fw-bold mt-5 mb-md-5 pb-4">Revealify</div>
 
                     <h3 class="mb-3 fw-bold fs-2 auth-title text-uppercase">User Login</h3>
 
-                    <h5 class="auth-sub-title mb-5 pb-3">Welcome to website</h5>
+                    <h5 class="auth-sub-title mb-md-5 mb-3 pb-3">Welcome to website</h5>
 
                     <div class="form-group form-floating mb-4 pb-2">
                         <input type="email" class="form-control shadow-none rounded-0" id="email" name="email"
