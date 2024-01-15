6352eeda5650d6b8e667c599f3c114d0b6019e73
Noyon Ahammed <noyon.redishketch@gmail.com>
2024-01-15 19:07:03 +0600

    banner, service, about section designed in landing page

--- resources/views/landing-page/pages/home.blade.php
+++ resources/views/landing-page/pages/home.blade.php
@@ -1,4 +1,167 @@
 @extends('landing-page.layout.layout')
 @section('content')
-    <div id="#home">home</div>
+    <div class="home">
+        {{--banner section start--}}
+        <section class="banner">
+            <div class="container">
+                <div class="content">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="left-content">
+                                <div class="promo  rounded-pill">
+                                    <img src="{{asset('/images/landing-page/dollar.svg')}}" alt="">
+                                    <span>Discover The Easiest ways to Build Your CV!</span>
+                                </div>
+                                <div class="title">Online CV Builder With Creative Templates.</div>
+                                <div class="desc">Our Perfect resume builder takes the hassle out of resume writing.
+                                    Choose
+                                    from several templates and follow easy prompts to create the perfect job-ready
+                                    resume.
+                                </div>
+                                <div class="buttons">
+                                    <button class="btn btn-orange-red me-3">choose template</button>
+                                    <button class="btn btn-orange-red">contact us</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="right-content">
+                                <img src="{{asset('/images/landing-page/banner.png')}}" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        {{--banner section end--}}
+
+
+        {{--service section start--}}
+        <section class="service" style="background-image: url('{{asset('/images/landing-page/service_banner.svg')}}')">
+            <div class="container">
+                <div class="content">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="left-content">
+                                <div class="service-item">
+                                    <div class="icon">
+                                        <img src="{{asset('/images/landing-page/d1.png')}}" alt="">
+                                    </div>
+                                    <div class="info">
+                                        <div class="title">Easy Online Resume Builder</div>
+                                        <div class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                            Laudantium modi assumenda.
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="service-item">
+                                    <div class="icon">
+                                        <img src="{{asset('/images/landing-page/d2.png')}}" alt="">
+                                    </div>
+                                    <div class="info">
+                                        <div class="title">Step By Step Expert Tips</div>
+                                        <div class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit skaj
+                                            gjska consectetur adipisicing elit.
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="service-item">
+                                    <div class="icon">
+                                        <img src="{{asset('/images/landing-page/d3.png')}}" alt="">
+                                    </div>
+                                    <div class="info">
+                                        <div class="title">Recruiter Approved Phrases</div>
+                                        <div class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                            Laudantium modi.
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="right-content">
+                                <div class="colors">
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                </div>
+                                <div class="section-title">Why Choose Our Platform?</div>
+                                <div class="desc">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at dictum risus, non
+                                    suscipit arcu. Quisque aliquam posuere tortor, sit amet convallis nunc scelerisque
+                                    in. <br> <br>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore eius molestiae
+                                    facere, natus reprehenderit eaque eum, autem ipsam. Magni, error. Tempora odit
+                                    laborum iure inventore possimus laboriosam qui nam. Fugit!
+                                </div>
+                                <button class="btn btn-orange-red">lets build your cv</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        {{--service section end--}}
+
+
+        {{--about section start--}}
+        <section class="about">
+            <div class="container">
+                <div class="content">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="left-content">
+                                <div class="colors">
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                </div>
+                                <div class="section-title">We Deliver The Best</div>
+                                <ul class="desc">
+                                    <li>
+                                        <img src="{{asset('images/landing-page/check.png')}}" alt="">
+                                        Proven CV Templates to increase Hiring Chance
+                                    </li>
+                                    <li>
+                                        <img src="{{asset('images/landing-page/check.png')}}" alt="">
+                                        Creative and Clean Templates Design
+                                    </li>
+                                    <li>
+                                        <img src="{{asset('images/landing-page/check.png')}}" alt="">
+                                        Easy and Intuitive Online CV Builder
+                                    </li>
+                                    <li>
+                                        <img src="{{asset('images/landing-page/check.png')}}" alt="">
+                                        Free to use. Developed by hiring professionals.
+                                    </li>
+                                    <li>
+                                        <img src="{{asset('images/landing-page/check.png')}}" alt="">
+                                        Fast Easy CV and Resume Formatting
+                                    </li>
+                                    <li>
+                                        <img src="{{asset('images/landing-page/check.png')}}" alt="">
+                                        Recruiter Approved Phrases.
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="right-content text-center">
+                                <img src="{{asset('/images/landing-page/cv.png')}}" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        {{--about section end--}}
+    </div>
 @endsection
--- resources/views/landing-page/shared/header.blade.php
+++ resources/views/landing-page/shared/header.blade.php
@@ -1,1 +1,21 @@
-<header>Header</header>
+<header class="fixed-top">
+    <div class="container">
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                    <a class="navbar-brand" href="#">
+                        <img src="{{asset('/images/landing-page/logo.png')}}" alt="">
+                        <span>CV Builder.</span>
+                    </a>
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 flex-grow-1 justify-content-end">
+                        <button class="btn btn-orange-red rounded-pill me-3">Log in</button>
+                        <button class="btn btn-orange-red rounded-pill">Sign up</button>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+</header>
