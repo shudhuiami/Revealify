cb65931d608fc9e830d4b92f19424f4ed94bccbe
Noyon Ahammed <noyon.redishketch@gmail.com>
2024-01-25 15:11:49 +0600

    loader svg image added in auth

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -67,18 +67,7 @@
                     <button type="submit" class="btn btn-orange-red px-4 rounded-0 mt-5 mb-4" v-if="!loading">Signup
                     </button>
                     <button type="button" class="btn btn-orange-red px-4 rounded-0 mt-5 mb-4" v-if="loading">
-                        <svg viewBox="0 0 24 24" width="16" height="16" stroke="currentColor"
-                             stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"
-                             class="css-i6dzq1 la-spin spin">
-                            <line x1="12" y1="2" x2="12" y2="6"></line>
-                            <line x1="12" y1="18" x2="12" y2="22"></line>
-                            <line x1="4.93" y1="4.93" x2="7.76" y2="7.76"></line>
-                            <line x1="16.24" y1="16.24" x2="19.07" y2="19.07"></line>
-                            <line x1="2" y1="12" x2="6" y2="12"></line>
-                            <line x1="18" y1="12" x2="22" y2="12"></line>
-                            <line x1="4.93" y1="19.07" x2="7.76" y2="16.24"></line>
-                            <line x1="16.24" y1="7.76" x2="19.07" y2="4.93"></line>
-                        </svg>
+                        <img :src="'/images/loader.svg'" class="spin" alt="loader">
                     </button>
 
                     <br>
